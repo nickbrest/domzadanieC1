4bb5f754e8dd7f2ea892758c095a4b29bc33d3be
Nick <mikabrmail@gmail.com>
2022-08-07 23:45:33 +0300

    task7 complete

--- task7/Program.cs
+++ task7/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Показать вторую цифру трёхзначного числа
-int numb = 459;
-int delnumb = numb / 10;
+int number = 459;
+int delnumb = number / 10;
 int numb2 = delnumb % 10;
 Console.WriteLine(numb2);
