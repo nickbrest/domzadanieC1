7a40b79e557122c46233dd28507507daa54fc944
Nick <mikabrmail@gmail.com>
2022-08-07 23:56:22 +0300

    task8 complete

---
+++ task8/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int number = new Random().Next(10, 99);
+int numberA = number / 10;
+int numberB = number % 10;
+int max = numberA;
+if (numberB > numberA) max = numberB;
+Console.WriteLine(number);
+Console.WriteLine(max);
