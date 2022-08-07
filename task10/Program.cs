078d15b78f0b88e7e94210c61e780695193fdb80
Nick <mikabrmail@gmail.com>
2022-08-08 00:09:09 +0300

    task10 complete

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+int numberA = 80;
+int numberB = 7;
+if (numberA % numberB == 0)
+{
+    Console.WriteLine("Число А кратно числу В");
+}
+else
+{
+    Console.WriteLine(numberA % numberB);
+}
